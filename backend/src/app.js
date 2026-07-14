4f8ab50c015f7c7a61dd56a4ac53a41abbfbaded
falguni-mondal <falgunimondal.job@gmail.com>
2026-07-14 19:06:18 +0530

    corrected the app.js CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -60,7 +60,7 @@ app.use(parser());
 
 // Dynamic CORS (Controlled via .env)
 // e.g., in .env: ALLOWED_ORIGINS=http://localhost:5173,https://mritsna.com
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : [process.env.FRONTEND_URL, "http://localhost:5174"];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : [process.env.FRONTEND_URL, process.env.ADMIN_FRONTEND_URL];
 
 app.use(
   cors({
